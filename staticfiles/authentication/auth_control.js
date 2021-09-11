5802670358febcdfa9c622ca3b010afbb1d003ef
Rares Mosescu <raresmose@gmail.com>
2021-09-11 07:51:03 +0000

    requirements.txt cleaned packages that get installed with the main ones

--- staticfiles/authentication/auth_control.js
+++ staticfiles/authentication/auth_control.js
@@ -1,74 +1,22 @@
-function register(email, password) {
-  firebase.auth().createUserWithEmailAndPassword(email, password)
-  .then((userCredential) => {
-    // Signed in 
-    var user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ..
-  });
-}
-
-function login(email, password) {
-  firebase.auth().signInWithEmailAndPassword(email, password)
-    .then((userCredential) => {
-      // Signed in
-      var user = userCredential.user;
-      return user;
-    })
-    .catch((error) => {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      console.log(errorCode + "\n" + errorMessage);
-      return false;
-    });
-}
-
-
-function logout() {
-  firebase.auth().signOut().then(() => {
-    // Sign-out successful.
-  }).catch((error) => {
-    // An error happened.
-  });
-}
-
-
-function isAuthenticated() {
-  const user = firebase.auth().currentUser;
-  if (user) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-function grantAccess(user) {
-  if (user) {
-    // If there's any login-status elements on page, update them with the login status. If not, skip to the next block of code.
-    login_status = document.getElementById("login-status");
-    if (login_status) {
-        login_status.innerHTML = "You're logged in as: " + user.email;
+function check_password() {
+    p1 = document.getElementById('password').value;
+    p2 = document.getElementById('password2').value;
+    if (p1.length >= 8 && p2.length >= 8) {
+        if (p1 == p2) {
+            document.getElementById('password-match-fail').style.display = "none";
+            document.getElementById('password-match-success').style.display = "block";
+            document.getElementById('submit-button').disabled = false;
+        }
+        else {
+            document.getElementById('password-match-fail').innerHTML = "Passwords don't match"
+            document.getElementById('submit-button').disabled = true;
+            document.getElementById('password-match-fail').style.display = "block";
+            document.getElementById('password-match-success').style.display = "none";
+        }
     }
-
-    // show all .authenticated elements and remove all .not-authenticated elements
-    document.querySelectorAll('.authenticated').forEach(elem => {
-        elem.style.display = "block"
-    });
-    document.querySelectorAll('.not-authenticated').forEach(elem => {
-        elem.style.display = "none"
-    }); 
-    } else {
-    document.getElementById("message").innerHTML = "No user signed in.";
-    document.querySelectorAll('.authenticated').forEach(elem => {
-        elem.style.display = "none"
-    });
-    document.querySelectorAll('.not-authenticated').forEach(elem => {
-        elem.style.display = "block"
-    });
+    else {
+        document.getElementById('password-match-fail').style.display = "block";
+        document.getElementById('password-match-success').style.display = "none";
+        document.getElementById('password-match-fail').innerHTML = "Password must contain at least 8 characters"
     }
-
 }
--- staticfiles/authentication/auth_state_listener.js
+++
@@ -1,3 +0,0 @@
-firebase.auth().onAuthStateChanged(function(user) {
-    
-});
